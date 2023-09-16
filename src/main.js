9ccf1c0d455bb55c8c61405b2b7b941b8de9812c
ViniciuMota <Vinimora44@gmail.com>
2023-09-16 10:02:16 -0300

    adicionado faq, footer e header

--- src/main.js
+++ src/main.js
@@ -1,5 +1,19 @@
 document.addEventListener('DOMContentLoaded', function () {
     const buttons = document.querySelectorAll('[data-tab-button]');
+    const questions = document.querySelectorAll('[data-faq-question]');
+
+    const heroSection = document.querySelector('.hero');
+    const alturaHero = heroSection.clientHeight;
+
+    window.addEventListener('scroll', function () {
+        const posicaoAtual = window.scrollY;
+
+        if (posicaoAtual < alturaHero) {
+            ocultaElementosHeader()
+        } else {
+            exibeElementoHeader()
+        }
+    })
 
     for (let i = 0; i < buttons.length; i++) {
         buttons[i].addEventListener('click', function (botao) {
@@ -12,7 +26,30 @@ document.addEventListener('DOMContentLoaded', function () {
 
         })
     }
+
+    for (let i = 0; i < questions.length; i++) {
+        questions[i].addEventListener('click', abreOuFechaResposta)
+
+    }
 })
+
+function ocultaElementosHeader() {
+    const header = document.querySelector('header');
+    header.classList.add('header--is-hidden')
+}
+
+function exibeElementoHeader() {
+    const header = document.querySelector('header');
+    header.classList.remove('header--is-hidden')
+
+}
+
+function abreOuFechaResposta(elemento) {
+    const classe = 'faq__questions__item--is-open'
+    const elementoPai = elemento.target.parentNode;
+
+    elementoPai.classList.toggle(classe)
+}
 
 function escondeTodasAbas() {
     const tabContainer = document.querySelectorAll('[data-tab-id]');
